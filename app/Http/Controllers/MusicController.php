7ec54e733cd620de966f609b46e098e090a5a376
Steven Joenior Naibaho <joeniorxd@gmail.com>
2024-08-13 23:40:38 +0700

    miyaw

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use FFMpeg;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use App\Events\MusicProcessingProgress;
 
 class MusicController extends Controller
 {
@@ -15,30 +17,45 @@ class MusicController extends Controller
 
     public function processMusic(Request $request)
     {
+        Log::info('Processing music upload');
+
         // Validasi file
         $request->validate([
             'musicFile' => 'required|mimes:mp3|max:10240', // Maks 10MB
         ]);
+
+        Log::info('File validated');
 
         // Upload file
         $file = $request->file('musicFile');
         $originalFilename = $file->getClientOriginalName();
         $filePath = $file->storeAs('uploads', $originalFilename);
 
+        Log::info('File uploaded to ' . $filePath);
+
         // Proses file untuk menambahkan efek bass boost
         $outputFilename = 'bassboosted_' . $originalFilename;
         $outputPath = 'processed/' . $outputFilename;
 
+        Log::info('Processing file with FFMpeg');
+
         FFMpeg::fromDisk('local')
             ->open($filePath)
-            ->addFilter('-af', 'bass=g=10')
+            ->addFilter(['-af', 'bass=g=10'])
             ->export()
             ->toDisk('local')
             ->inFormat(new \FFMpeg\Format\Audio\Mp3)
+            ->onProgress(function ($percentage) {
+                event(new MusicProcessingProgress($percentage));
+            })
             ->save($outputPath);
+
+        Log::info('File processed and saved to ' . $outputPath);
 
         // Hapus file asli (opsional)
         Storage::disk('local')->delete($filePath);
+
+        Log::info('Original file deleted');
 
         // Kembalikan link untuk mengunduh file yang sudah diproses
         return response()->download(storage_path("app/$outputPath"))->deleteFileAfterSend(true);
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,6 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload Music</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            Pusher.logToConsole = true;
+
+            var pusher = new Pusher('{{ env('PUSHER_APP_KEY') }}', {
+                cluster: '{{ env('PUSHER_APP_CLUSTER') }}',
+                forceTLS: true
+            });
+
+            var channel = pusher.subscribe('music-processing');
+            channel.bind('App\\Events\\MusicProcessingProgress', function(data) {
+                var progressText = 'Progress: ' + data.progress + '%';
+                document.getElementById('progress').innerText = progressText;
+                toastr.info(progressText);
+            });
+        });
+    </script>
 </head>
 <body>
     <div class="container mt-5">
@@ -17,6 +38,7 @@
             </div>
             <button type="submit" class="btn btn-primary">Upload and Process</button>
         </form>
+        <div id="progress" class="mt-3"></div>
     </div>
 </body>
 </html>
